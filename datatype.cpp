c9309d5ccc693d6ac873904a0ebf91ae1423a984
Siddhi Mahesh Dixit <71143096+Siddhi1101@users.noreply.github.com>
2021-06-16 23:16:25 -0700

    add file
    add files

---
+++ datatype.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+
+  int Num = 99;
+  float FloatNum = 88.25;
+  double DoubleNum = 9.98;
+  char Letter = 'A';
+  bool Boolean = true;
+  string myString = "Hello";
+
+
+  cout << "int: " << Num << "\n";
+  cout << "float: " << FloatNum << "\n";
+  cout << "double: " << DoubleNum << "\n";
+  cout << "char: " << Letter << "\n";
+  cout << "bool: " << Boolean << "\n";
+  cout << "string: " << String << "\n";
+
+  return 0;
+}
---
+++ math.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << sqrt(100) << "\n";
+  cout << log(2) << "\n";
+  cout << min(5, 10)<<endl;
+  cout << max(100,81);
+  return 0;
+}
